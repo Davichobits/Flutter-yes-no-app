524ca6225293c9a0bb3ef7c4ef939563dddf5a1d
Davichobits <davidruizdeveloper@gmail.com>
2024-04-18 10:29:09 -0500

    Adding an image to the message

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 5),
+        _ImageBubble(),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+          'https://yesno.wtf/assets/no/8-5e08abbe5aacd2cf531948145b787e9a.gif',
+          width: size.width * 0.7,
+          height: 250,
+          fit: BoxFit.cover),
+    );
+  }
+}
