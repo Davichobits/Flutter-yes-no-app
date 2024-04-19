2fc3af3dc002eba659228654f4fa4410d5158d60
Davichobits <davidruizdeveloper@gmail.com>
2024-04-19 15:52:11 -0500

    Keyboard controller

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,29 +5,43 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+
+    final FocusNode focusNode = FocusNode();
+
     final colors = Theme.of(context).colorScheme;
+
     final outLineInputBorder = UnderlineInputBorder(
       borderSide: BorderSide(color: colors.primary),
       borderRadius: BorderRadius.circular(20),
     );
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
       filled: true,
       enabledBorder: outLineInputBorder,
       focusedBorder: outLineInputBorder,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send_outlined),
-        onPressed: () {},
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('Button $textValue');
+          textController.clear();
+        },
       ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('submited value: $value');
-      },
-      onChanged: (value) {
-        print('current valud $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
